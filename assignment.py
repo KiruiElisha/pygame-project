88b4b1b5e952a406db2c9a422eaffed2fe51b178
Elisha <ronoelisha625@gmail.com>
2022-11-06 17:53:13 +0300

    Added Buttons

--- assignment.py
+++ assignment.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
-import pygame  
+import pygame
+import sys
 from math import pi  
 pygame.init()  
 
 window = pygame.display.set_mode((800,700))
 window.fill((255, 255, 255))
+
+fps = 60
+fpsClock = pygame.time.Clock()
+
+font = pygame.font.SysFont('Arial', 20)
 
 class Head:  
   def head():
@@ -59,14 +65,81 @@ class Robot():
     hands = Hands.left_hand() and Hands.right_hand()    
     legs = Legs.legs()
 
+objects = []
+class Button():
+    def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.onclickFunction = onclickFunction
+        self.onePress = onePress
 
+        self.fillColors = {
+            'normal': '#d413ed',
+            'hover': '#666666',
+            'pressed': '#333333',
+        }
+
+        self.buttonSurface = pygame.Surface((self.width, self.height))
+        self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+        self.buttonSurf = font.render(buttonText, True, (20, 20, 20))
+
+        self.alreadyPressed = False
+
+        objects.append(self)
+
+    def process(self):
+
+        mousePos = pygame.mouse.get_pos()
+        
+        self.buttonSurface.fill(self.fillColors['normal'])
+        if self.buttonRect.collidepoint(mousePos):
+            self.buttonSurface.fill(self.fillColors['hover'])
+
+            if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                self.buttonSurface.fill(self.fillColors['pressed'])
+
+                if self.onePress:
+                    self.onclickFunction()
+
+                elif not self.alreadyPressed:
+                    self.onclickFunction()
+                    self.alreadyPressed = True
+
+            else:
+                self.alreadyPressed = False
+
+        self.buttonSurface.blit(self.buttonSurf, [
+            self.buttonRect.width/2 - self.buttonSurf.get_rect().width/2,
+            self.buttonRect.height/2 - self.buttonSurf.get_rect().height/2
+        ])
+        window.blit(self.buttonSurface, self.buttonRect)
+
+def myFunction():
+    print('Animated')
+
+
+
+
+
+AnimateButton = Button(400, 500, 150, 80, 'Animate', myFunction)
+ClapButton = Button(620, 500, 150, 80, 'Clap', myFunction, True)
 robot = Robot()
-
 pygame.display.update()
 pygame.display.flip()
+
 running = True
 while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    for object in objects:
+        object.process()
+
+    pygame.display.flip()
+    fpsClock.tick(fps)
 
